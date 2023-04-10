a704599c288c2331df0fecfa21240b07b4c8273b
Simon-157 <boatengsimonjnr157@gmail.com>
2023-04-10 21:02:32 +0000

    ci/cd

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-// EMAIL AND PASSWORD SIGN IN
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
